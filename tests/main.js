b23b7b614a8794dfe1df47e1db6a3d99b5256273
Mr McHugh <developer@yrucalling.me>
2012-07-27 10:54:08 +1000

    Update tests to work with node style callbacks

--- tests/main.js
+++ tests/main.js
@@ -11,33 +11,33 @@ var assert = require('assert'),
     yql    = require('yql');
 
 // Example #1 - Param binding
-new yql.exec("SELECT * FROM weather.forecast WHERE (location = @zip)", function(response) {
+new yql.exec("SELECT * FROM weather.forecast WHERE (location = @zip)", {"zip": 90066}, function(error, response) {
     var testID = "1";
     try {
         assert.ok(response.query.results);
         pass(testID);
     }
     catch (e) { return fail(testID, e); }
-}, {"zip": 90066});
+});
 
 
 // Example #2 - Param binding + SSL
-new yql.exec("SELECT * FROM html WHERE url = @url", function(response) {
+new yql.exec("SELECT * FROM html WHERE url = @url", {url:"http://www.yahoo.com"}, {ssl:true}, function(error, response) {
     var testID = "2";
     try {
         assert.ok(response.query.count);
         pass(testID);
     }
     catch (e) { return fail(testID, e); }
-}, {url:"http://www.yahoo.com"}, {ssl:true});
+});
 
 
 // Example #3 - Non-existent table
-new yql.exec("SELECT * FROM foobar.badTable", function(response) {
+new yql.exec("SELECT * FROM foobar.badTable", function(error, response) {
     var testID = "3";
     try {
-        // response.error.description will only exist if YQL returns an error
-        assert.ok(response.error.description);
+        // error object will be set if a YQL error occured
+        assert.ok(error);
         pass(testID);
     }
     catch (e) { return fail(testID, e); }
@@ -45,11 +45,11 @@ new yql.exec("SELECT * FROM foobar.badTable", function(response) {
 
 
 // Example #4 - Missing required fields
-new yql.exec("SELECT * FROM html", function(response) {
+new yql.exec("SELECT * FROM html", function(error, response) {
     var testID = "4";
     try {
-        // response.error.description will only exist if YQL returns an error
-        assert.ok(response.error.description);
+        // error object will be set if a YQL error occured
+        assert.ok(error);
         pass(testID);
     }
     catch (e) { return fail(testID, e); }
